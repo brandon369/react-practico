e74e4b779385c7e05d6e6f341d898dbc9928a27e
Brandon A <byarrieta@protonmail.com>
2022-02-10 22:03:50 -0500

    components ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import ProductList from '../containers/ProductList';
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+	return (
+		<>
+			<ProductList />
+		</>
+	);
 }
 
-export default Home
+export default Home;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../styles/global.css'
-import Login from "../containers/Login";
 import Layout from "../containers/Layout";
-import RecoveryPassword from "../containers/RecoveryPassword";
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import SendEmail from "../pages/SendEmail";
+import NewPassword from "../pages/NewPassword";
+import MyAccount from "../pages/MyAccount";
+import CreateAccount from "../pages/CreateAccount";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
+import Login from "../pages/Login";
+import RecoveryPassword from "../pages/RecoveryPassword";
 
 const App = () => {
   return (
@@ -17,7 +23,15 @@ const App = () => {
           <Route exact path={'/'} element={<Home/>}/>
           <Route exact path={'/login'} element={<Login/>}/>
           <Route exact path={'/recovery-password'} element={<RecoveryPassword/>}/>
+
+          <Route exact path="/send-email" element={<SendEmail/>} />
+          <Route exact path="/new-password" element={<NewPassword/>} />
+          <Route exact path="/account" element={<MyAccount/>} />
+          <Route exact path="/signup" element={<CreateAccount/>} />
+          <Route exact path="/checkout" element={<Checkout/>} />
+          <Route exact path="/orders" element={<Orders/>} />
           <Route path='*' element={<NotFound/>}/>
+
         </Routes>
       </Layout>
     </BrowserRouter>
